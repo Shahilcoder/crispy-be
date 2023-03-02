b696fcc2bf754260115bf8e111de98a3c4faaccf
MD Yasin <85479838+awesome-yasin@users.noreply.github.com>
2023-03-02 22:11:01 +0530

    created route for message send and retrive

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,68 @@
 const express = require('express');
 const { signup, login } = require('../controllers/auth.controllers');
 const authRouter = express.Router();
-
+const messagesRouter = express.Router();
 authRouter.post('/signup', signup);
 authRouter.post('/login', login);
 
-module.exports = authRouter;
+//code by yasin
+// Route for sending message
+
+messagesRouter.post('/', authMiddleware, async (req, res) => {
+  try {
+    const { message, receiverId } = req.body;
+    const senderId = req.user.uid;
+
+    const messagesRef = db.collection('messages');
+    const newMessage = {
+      senderId,
+      receiverId,
+      message,
+      createdAt: new Date().toISOString()
+    };
+
+    await messagesRef.add(newMessage);
+
+    res.status(201).json({ message: 'Message sent successfully' });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to send message' });
+  }
+});
+
+//code by yasin
+// Route for sending message
+//end
+
+
+//code by yasin
+//route for retriving all message of auth user
+messagesRouter.get('/', authMiddleware, async (req, res) => {
+    try {
+      const messages = [];
+      const messagesRef = db.collection('messages');
+      const querySnapshot = await messagesRef.orderBy('timestamp').get();
+  
+      querySnapshot.forEach((doc) => {
+        const messageData = doc.data();
+        messages.push({
+          id: doc.id,
+          sender: messageData.sender,
+          text: messageData.text,
+          timestamp: messageData.timestamp.toMillis(),
+        });
+      });
+  
+      res.status(200).json(messages);
+    } catch (error) {
+      console.error('Error getting messages:', error);
+      res.status(500).json({ error: 'Error getting messages' });
+    }
+});
+//code by yasin
+//route for retriving all message of auth user
+//end
+
+module.exports = {
+   messagesRouter,
+   authRouter
+}
